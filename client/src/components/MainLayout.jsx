87afc0a290651e9df5b9c824ef645f1a4837137a
Daniel Lesko <daniel.lesko@publicisgroupe.cz>
2024-05-30 22:22:17 +0200

    fixed top navigation

--- client/src/components/MainLayout.jsx
+++ client/src/components/MainLayout.jsx
@@ -1,12 +1,23 @@
 import React from "react";
-import { Outlet } from "react-router";
+import { Outlet, useLocation } from "react-router";
 import MainMenu from "./MainMenu";
+import { menuData } from "../data/data";
 
 const MainLayout = () => {
+  const location = useLocation();
+
+  const currentMenuItem = menuData.find(
+    (item) => item.link === location.pathname
+  );
+
+  console.log("location", location.pathname);
+
   return (
     <div className="flex flex-col pl-10 pt-4 bg-zinc-100 h-screen">
       <div>
-        <h1 className="text-black text-4xl pb-10">Moje objednávky</h1>
+        <h1 className="text-black text-4xl pb-10">
+          {currentMenuItem ? currentMenuItem.name : ""}
+        </h1>
       </div>
       <div className="flex w-full">
         <div className="w-1/5">
--- client/src/data/data.js
+++ client/src/data/data.js
@@ -4,7 +4,7 @@ import { CiHeart } from "react-icons/ci";
 
 export const menuData = [
   { id: 1, name: "Moje objednávky", link: "/", icon: <CiFileOn /> },
-  { id: 2, name: "faktúry", link: "/faktury", icon: <LuFileCheck /> },
+  { id: 2, name: "Faktúry", link: "/faktury", icon: <LuFileCheck /> },
   { id: 3, name: "Seznam přáni", link: "/seznam-prani", icon: <CiHeart /> },
   { id: 4, name: "Slevové kódy", link: "/slevove-kody", icon: <CiHeart /> },
   {
