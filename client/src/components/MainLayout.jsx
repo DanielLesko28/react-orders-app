4040113731ff75abada2e4d50b11e695a68e6cb2
Daniel Lesko <daniel.lesko@publicisgroupe.cz>
2024-05-31 10:03:16 +0200

    working on responsivity

--- client/src/components/MainLayout.jsx
+++ client/src/components/MainLayout.jsx
@@ -13,17 +13,17 @@ const MainLayout = () => {
   console.log("location", location.pathname);
 
   return (
-    <div className="flex flex-col p-4 sm:pl-10 sm:pt-4 bg-zinc-100 min-h-screen">
+    <div className="flex flex-col p-4 md:pl-10 md:pt-4 bg-zinc-100 min-h-screen">
       <div>
-        <h1 className="text-black text-2xl sm:text-4xl pb-6 sm:pb-10">
+        <h1 className="text-black text-2xl md:text-4xl pb-6 md:pb-10">
           {currentMenuItem ? currentMenuItem.name : ""}
         </h1>
       </div>
-      <div className="flex flex-col sm:flex-row w-full">
-        <div className="w-full sm:w-1/5 mb-4 sm:mb-0">
+      <div className="flex flex-col md:flex-row w-full">
+        <div className="w-full md:w-1/4 lg:w-1/5 mb-4 md:mb-0">
           <MainMenu />
         </div>
-        <div className="w-full sm:w-4/5 sm:ml-4">
+        <div className="w-full md:w-3/4 lg:w-4/5 md:ml-4">
           <Outlet />
         </div>
       </div>
